906d23804aaff668ae6a1eca37df96079da22382
Zeynep Caglar <48796179+caglarze@users.noreply.github.com>
2019-06-15 10:17:08 +0200

    Add files via upload

--- Tut10_MeshCompleted/MeshExample.cs
+++ Tut10_MeshCompleted/MeshExample.cs
@@ -45,7 +45,7 @@ namespace Fusee.Tutorial.Core
                            // SHADER EFFECT COMPONENT
                             new ShaderEffectComponent
                             {
-                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0.7f, 0.7f, 0.7f), new float3(1, 1, 1), 5)
+                                Effect = SimpleMeshes.MakeShaderEffect(new float3(1f, 0.76f, 0.76f), new float3(1, 1, 1), 5)
                             },
 
                             // MESH COMPONENT
@@ -61,7 +61,7 @@ namespace Fusee.Tutorial.Core
         public override void Init()
         {
             // Set the clear color for the backbuffer to white (100% intentsity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+            RC.ClearColor = new float4(0.84f, 0.84f, 0.84f, 1);
 
             _scene = CreateScene();
 
@@ -72,13 +72,13 @@ namespace Fusee.Tutorial.Core
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+            _baseTransform.Rotation = new float3(2, M.MinAngle(TimeSinceStart), 0);
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+            RC.View = float4x4.CreateTranslation(0, 0, 50) * float4x4.CreateRotationX(-(float) Atan(10.0 / 60.0));
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
